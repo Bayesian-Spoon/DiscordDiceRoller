3245fb387e267bb06d91dccb30d4114397a324f6
Melissa Bollen <bayesianspoon@gmail.com>
2024-02-16 20:45:30 -0800

    Added RollType and a second type of roll for a new RPG (Blades In The Dark)

--- DiscordDiceRoller/DiceRoller.cs
+++ DiscordDiceRoller/DiceRoller.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +11,24 @@ namespace DiscordDiceRoller
 {
     public class DiceRoller
     {
+        public enum RollType { Regular, BITD, Insomnia };
         public class RollResult
         {
+            public readonly RollType type;
             public string? DiceRolled;
             public List<int> IndividualRolls = new List<int>();
             public string? DetailedOutcome;
             public int? NumericOutcome;
+
+            public RollResult(RollType _type)
+            {
+                type = _type;
+            }
+
+            public string IndividualRollsJoined()
+            {
+                return string.Join(", ", IndividualRolls);
+            }
         }
 
         /// <summary>
@@ -38,7 +53,7 @@ namespace DiscordDiceRoller
 
         public static RollResult DetailedRoll(int dice, int sides)
         {
-            RollResult result = new RollResult();
+            RollResult result = new RollResult(RollType.Regular);
             result.DiceRolled = $"{dice}d{sides}";
 
             for (int die = 0; die < dice; die++)
@@ -47,7 +62,7 @@ namespace DiscordDiceRoller
             }
 
             result.NumericOutcome = result.IndividualRolls.Sum();
-            result.DetailedOutcome = $"{result.NumericOutcome} ({String.Join(", ", result.IndividualRolls)})";
+            result.DetailedOutcome = $"{result.NumericOutcome} ({result.IndividualRollsJoined()})";
 
             return result;
         }
@@ -71,6 +86,52 @@ namespace DiscordDiceRoller
                 }
 
             }
+        }
+
+        public static RollResult BladesInTheDark(int dice)
+        { 
+            RollResult result = new RollResult(RollType.Regular);
+            result.DiceRolled = $"{dice}d6, Blades In The Dark style";
+
+            //Roll dice
+            for (int die = 0; die < dice; die++)
+            {
+                result.IndividualRolls.Add(Roll(6));
+            }
+
+            //Calculate outcome
+            result.NumericOutcome= result.IndividualRolls.Max();
+            switch(result.NumericOutcome)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    result.DetailedOutcome = "Fail";
+                    break;
+                case 4:
+                case 5:
+                    result.DetailedOutcome = "Mixed";
+                    break;
+                case 6:
+                    //Multiple sixes produces a critical
+                    if (result.IndividualRolls.Count(x => x == 6) > 1)
+                    {
+                        result.DetailedOutcome = "Critical";
+                    }
+                    else
+                    {
+                        result.DetailedOutcome = "Success";
+                    }
+                    break;
+                default:
+                    throw new ArgumentException($"Numeric Outcome was {result.NumericOutcome}, but should be 1-6");
+            }
+
+            //Append details to outcome
+            result.DetailedOutcome += $" ({result.IndividualRollsJoined()})";
+
+            //return
+            return result;
         }
     }
 }
