09721dc47ad66698c6c5522d727f20ede70aeb68
Melissa Bollen <bayesianspoon@gmail.com>
2024-03-13 09:33:17 -0700

    Added new function to determine the highest result amongst multiple dice pools, including unit tests

---
+++ DiscordDiceRoller-Tests/ut_DetermineHighestResult.cs
@@ -0,0 +1,88 @@
+﻿using DiscordDiceRoller;
+
+namespace DiscordDiceRoller_Tests
+{
+    [TestClass]
+    public class ut_DetermineHighestResult
+    {
+        [TestMethod]
+        public void ut_DHR_HighBeatsLow()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 6 });
+            categories.Add("Category 2", new List<int> { 5 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 1", result);
+        }
+
+        [TestMethod]
+        public void ut_DHR_ConsiderMultipleDice()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 6, 6, 6 });
+            categories.Add("Category 2", new List<int> { 6, 6, 5 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 1", result);
+        }
+
+        [TestMethod]
+        public void ut_DHR_ConsiderUnsortedDice()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 4, 5, 6 });
+            categories.Add("Category 2", new List<int> { 6, 3, 5 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 1", result);
+        }
+
+        [TestMethod]
+        public void ut_DHR_ResolveDifferentLengths()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 5 });
+            categories.Add("Category 2", new List<int> { 5, 1 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 2", result);
+        }
+
+        [TestMethod]
+        public void ut_DHR_ResolveUnsortedDifferentLengths()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 5 });
+            categories.Add("Category 2", new List<int> { 1, 5 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 2", result);
+        }
+
+        [TestMethod]
+        public void ut_DHR_ResolveFullTies()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 5, 5, 5 });
+            categories.Add("Category 2", new List<int> { 5, 5, 5 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 1", result);
+        }
+
+        [TestMethod]
+        public void ut_DHR_NumerousCategories()
+        {
+            Dictionary<string, List<int>> categories = new Dictionary<string, List<int>>();
+            categories.Add("Category 1", new List<int> { 6, 6, 6, 6 });
+            categories.Add("Category 2", new List<int> { 6, 6, 6, 5 });
+            categories.Add("Category 3", new List<int> { 6, 6, 5 });
+            categories.Add("Category 4", new List<int> { 6, 5 });
+            categories.Add("Category 5", new List<int> { 5 });
+
+            string result = DiceRoller.DetermineHighestResult(categories);
+            Assert.AreEqual("Category 1", result);
+        }
+    }
+}
--- DiscordDiceRoller/DiceRoller.cs
+++ DiscordDiceRoller/DiceRoller.cs
@@ -35,5 +35,86 @@ namespace DiscordDiceRoller
             }
             return result;
         }
+
+        /*
+        * Finds the highest pool amongst multiple possible dice pools. 
+        * In the case of a tie, the order within the original dictionary will be the tie-breaker.
+        * 
+        * Examples:
+        *  Discipline (6) beats Exhaustion (5, 4, 4) because it has the higher first result
+        *  Discipline (6,5) beats Exhaustion (6, 4) because it has the higher second result
+        *  Discipline (6, 1) beats Exhaustion (6) because Discipline has more dice
+        *  Discipline (6) beats Exhaustion (6) because it wins the tie-breaker
+        */
+        public static string DetermineHighestResult(Dictionary<string, List<int>> categories)
+        {
+            //Ensure our results are sorted
+            Dictionary<string, int[]> sortedCategories = new Dictionary<string, int[]>();
+            foreach (string category in categories.Keys)
+            {
+                sortedCategories.Add(category, categories[category].OrderByDescending(x => x).ToArray());
+            }
+
+            //Keep track of contenders
+            // Category names should always be unique, so we can use a HashSet here
+            HashSet<string> viableCategories = sortedCategories.Keys.ToHashSet();
+            HashSet<string> categoriesToRemove = new HashSet<string>();
+
+            int largestPool = categories.Values.Select(x => x.Count).Max();
+            for (int index = 0; index < largestPool; index++)
+            {
+                int highestDie = 0;
+                foreach (string category in viableCategories)
+                {
+                    if (sortedCategories[category].Length <= index)
+                    {
+                        //Remove if there's no more dice in this category
+                        categoriesToRemove.Add(category);
+                    }
+                    else if (sortedCategories[category][index] < highestDie)
+                    {
+                        //Remove if this isn't the highest roll
+                        categoriesToRemove.Add(category);
+                    }
+                    else
+                    {
+                        highestDie = sortedCategories[category][index];
+                    }
+                }
+
+                //Prune
+                foreach (string removal in categoriesToRemove)
+                {
+                    viableCategories.Remove(removal);
+                }
+                categoriesToRemove.Clear();
+
+                //Now that we know the real highest die, do another removal pass
+                foreach (string category in viableCategories)
+                {
+                    if (sortedCategories[category][index] < highestDie)
+                    {
+                        //Remove if this isn't the highest roll
+                        categoriesToRemove.Add(category);
+                    }
+                }
+
+                //Prune again
+                foreach (string removal in categoriesToRemove)
+                {
+                    viableCategories.Remove(removal);
+                }
+                categoriesToRemove.Clear();
+
+                //Check if we have a winner
+                if (viableCategories.Count == 1)
+                {
+                    return viableCategories.First();
+                }
+            }
+
+            //Ties are resolved based on the order within the original dictionary
+            return viableCategories.First();
+        }
     }
 }
