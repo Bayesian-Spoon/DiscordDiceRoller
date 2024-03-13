a6a75aedd3b60a83b3c6423d6ac243b0be6ab2f1
Melissa Bollen <bayesianspoon@gmail.com>
2024-03-13 09:32:16 -0700

    Cleanup using statements

--- DiscordDiceRoller/DiceParsers/FateDiceParser.cs
+++ DiscordDiceRoller/DiceParsers/FateDiceParser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static DiscordDiceRoller.DiceRoller;
+﻿using static DiscordDiceRoller.DiceRoller;
 
 namespace DiscordDiceRoller.DiceParsers
 {
